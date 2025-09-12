7a9ab1dfc2876113b7c00bc17451d768c4a8f1b5
ArdakUtepbergenov <98506975+ArdakUtepbergenov@users.noreply.github.com>
2025-09-13 03:59:18 +0500

    Update PavingCatalog.tsx

--- src/components/PavingCatalog.tsx
+++ src/components/PavingCatalog.tsx
@@ -26,24 +26,26 @@ import { ArrowUpDown, Grid3X3, ShoppingCart } from "lucide-react";
 // import img19 from "@/assets/19.JPG";
 
 import img1 from "@/assets/1.jpeg";
-import img2 from "@/assets/2.jpeg";
-import img3 from "@/assets/3.jpeg";
-import img4 from "@/assets/4.jpeg";
+import img2 from "@/assets/2.jpg";
+import img3 from "@/assets/3.jpg";
+import img4 from "@/assets/4.jpg";
 import img5 from "@/assets/5.jpeg";
-import img6 from "@/assets/6.jpeg";
-import img7 from "@/assets/7.jpeg";
-import img8 from "@/assets/8.jpeg";
-import img9 from "@/assets/9.jpeg";
-import img10 from "@/assets/10.jpeg";
-import img11 from "@/assets/11.jpeg";
-import img12 from "@/assets/12.jpeg";
-import img13 from "@/assets/13.jpeg";
-import img14 from "@/assets/14.jpeg";
-import img15 from "@/assets/15.jpeg";
-import img16 from "@/assets/16.jpeg";
+import img6 from "@/assets/6.jpg";
+import img7 from "@/assets/7.jpg";
+import img8 from "@/assets/8.jpg";
+import img9 from "@/assets/9.jpg";
+import img10 from "@/assets/10.jpg";
+import img11 from "@/assets/11.jpg";
+import img12 from "@/assets/12.jpg";
+import img13 from "@/assets/13.jpg";
+import img14 from "@/assets/14.jpg";
+import img15 from "@/assets/15.jpg";
+import img16 from "@/assets/16.jpg";
 import img17 from "@/assets/17.jpeg";
-import img18 from "@/assets/18.jpeg";
-import img19 from "@/assets/19.jpeg";
+import img18 from "@/assets/18.jpg";
+import img19 from "@/assets/19.jpg";
+import img19 from "@/assets/20.jpg";
+
 
 
 const PavingCatalog = () => {
