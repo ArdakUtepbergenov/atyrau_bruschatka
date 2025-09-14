b0622681c6ce825c8d8bb06d1a1be65c7e513bda
ArdakUtepbergenov <98506975+ArdakUtepbergenov@users.noreply.github.com>
2025-09-14 19:50:55 +0500

    Update PavingCatalog.tsx

--- src/components/PavingCatalog.tsx
+++ src/components/PavingCatalog.tsx
@@ -25,14 +25,14 @@ import { ArrowUpDown, Grid3X3, ShoppingCart } from "lucide-react";
 // import img18 from "@/assets/18.JPG";
 // import img19 from "@/assets/19.JPG";
 
-import img1 from "@/assets/";
+import img1 from "@/assets/brilliantseryi.jpeg";
 import img2 from "@/assets/2.jpg";
 import img3 from "@/assets/3.jpg";
 import img4 from "@/assets/4.jpg";
 import img5 from "@/assets/brilliant.jpeg";
 import img6 from "@/assets/6.jpg";
 import img7 from "@/assets/7.jpg";
-import img8 from "@/assets/8.jpg";
+import img8 from "@/assets/staryigorod.jpg";
 import img9 from "@/assets/9.jpeg";
 import img10 from "@/assets/vavilion.jpeg";
 import img11 from "@/assets/11.jpg";
