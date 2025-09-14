24ca0b15265a36de2eab1405be8d0f83bc612cea
ArdakUtepbergenov <98506975+ArdakUtepbergenov@users.noreply.github.com>
2025-09-14 21:44:51 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -95,9 +95,26 @@ const Footer = () => {
                 </div>
 
                 <div className="flex items-center gap-3">
-                  <Mail className="w-5 h-5 text-primary-foreground/60" />
+                  <svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="w-5 h-5 text-primary-foreground/60"
+  viewBox="0 0 24 24"
+  fill="currentColor"
+>
+  <path d="M7 2C4.243 2 2 4.243 2 7v10c0 2.757 
+    2.243 5 5 5h10c2.757 0 5-2.243 
+    5-5V7c0-2.757-2.243-5-5-5H7zm0 
+    2h10c1.654 0 3 1.346 3 
+    3v10c0 1.654-1.346 3-3 
+    3H7c-1.654 0-3-1.346-3-3V7c0-1.654 
+    1.346-3 3-3zm10 1a1 1 0 100 2 
+    1 1 0 000-2zM12 7a5 5 0 
+    100 10 5 5 0 000-10zm0 
+    2a3 3 0 110 6 3 3 0 010-6z" />
+</svg>
+
                   <div>
-                    <p className="text-primary-foreground">info@atyrau-paving.kz</p>
+                    <p className="text-primary-foreground">@atyrau_brushatka</p>
                   </div>
                 </div>
               </div>
