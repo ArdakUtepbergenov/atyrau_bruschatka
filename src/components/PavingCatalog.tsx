5e172e57c2e4911a5bb1b9773f5c59c696c46a90
ArdakUtepbergenov <98506975+ArdakUtepbergenov@users.noreply.github.com>
2025-09-14 19:53:10 +0500

    Update PavingCatalog.tsx

--- src/components/PavingCatalog.tsx
+++ src/components/PavingCatalog.tsx
@@ -29,7 +29,7 @@ import img1 from "@/assets/brilliantseryi.jpeg";
 import img2 from "@/assets/2.jpg";
 import img3 from "@/assets/3.jpg";
 import img4 from "@/assets/4.jpg";
-import img5 from "@/assets/brilliant.jpeg";
+import img5 from "@/assets/brilliantfinal.jpeg";
 import img6 from "@/assets/6.jpg";
 import img7 from "@/assets/7.jpg";
 import img8 from "@/assets/staryigorod.jpg";
