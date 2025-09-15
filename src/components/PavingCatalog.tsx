595c0d63f51c709a7e6cb425560479e8e22a12ce
ArdakUtepbergenov <98506975+ArdakUtepbergenov@users.noreply.github.com>
2025-09-15 14:38:38 +0500

    Update PavingCatalog.tsx

--- src/components/PavingCatalog.tsx
+++ src/components/PavingCatalog.tsx
@@ -33,7 +33,7 @@ import img5 from "@/assets/brilliantfinal.jpeg";
 import img6 from "@/assets/6.jpg";
 import img7 from "@/assets/7.jpg";
 import img8 from "@/assets/staryigorod.jpeg";
-import img9 from "@/assets/9.jpeg";
+import img9 from "@/assets/30x30.jpg";
 import img10 from "@/assets/vavilion.jpeg";
 import img11 from "@/assets/11.jpg";
 import img12 from "@/assets/12.jpeg";
@@ -70,7 +70,7 @@ const PavingCatalog = () => {
   { id: 12, name: "Мрамор Вавилон", price: 6500, category: "classic", size: "Толщина 4 см", image: img20 },
 
   // остальные элементы пока без изменений
-  //{ id: 9, name: "Старый город", price: 7500, category: "classic",  size: "Толщина 4 см", image: img9 },
+  { id: 19, name: "Плитка 30х30", price: 3500, category: "classic",  size: "Толщина 3 см", image: img9 },
   { id: 10, name: "Вавилон желтый", price: 5000, category: "antique", size: "Толщина 4 см", image: img10 },
   //{ id: 12, name: "Брусчатка 12", price: 4600, category: "concrete", size: "350x350x50", image: img12 },
   { id: 13, name: "Вавилон", price: 4500, category: "geometric", size: "Толщина 4 см", image: img13 },
