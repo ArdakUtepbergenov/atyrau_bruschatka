829451ea9463fb805a4d68ac27f15fb5e982e1aa
ArdakUtepbergenov <98506975+ArdakUtepbergenov@users.noreply.github.com>
2025-09-15 14:11:23 +0500

    Update PavingCatalog.tsx

--- src/components/PavingCatalog.tsx
+++ src/components/PavingCatalog.tsx
@@ -70,7 +70,7 @@ const PavingCatalog = () => {
   { id: 12, name: "Мрамор Вавилон", price: 6500, category: "classic", size: "Толщина 4 см", image: img20 },
 
   // остальные элементы пока без изменений
-  { id: 9, name: "Брусчатка 9", price: 7500, category: "classic",  size: "Толщина 4 см", image: img9 },
+  { id: 9, name: "Старый город", price: 7500, category: "classic",  size: "Толщина 4 см", image: img9 },
   { id: 10, name: "Вавилон желтый", price: 5000, category: "antique", size: "Толщина 4 см", image: img10 },
   //{ id: 12, name: "Брусчатка 12", price: 4600, category: "concrete", size: "350x350x50", image: img12 },
   { id: 13, name: "Брусчатка 13", price: 6500, category: "geometric", size: "Толщина 5 см", image: img13 },
