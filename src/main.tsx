4032d71fb3586ea4add0df2fd93be93c9531d3b8
ArdakUtepbergenov <98506975+ArdakUtepbergenov@users.noreply.github.com>
2025-09-21 23:22:42 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
-import "./index.css";
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App.tsx";
+// import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/atyrau_bruschatka">
+// createRoot(document.getElementById("root")!).render(
+//   <BrowserRouter basename="/atyrau_bruschatka">
+//     <App />
+//   </BrowserRouter>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <h1 style={{ textAlign: "center", marginTop: "50px" }}>React работает 🎉</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
